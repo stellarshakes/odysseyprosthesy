48327292ae5437c9af38b35a0d337ca31ed2d730
stellarshakes <stella.p.hatzis@gmail.com>
2019-10-08 14:49:32 +1100

    edited theveil

--- src/poemfiles/theveil.js
+++ src/poemfiles/theveil.js
@@ -1,37 +1,36 @@
 import React from 'react'
 import CycleSpan from "../CycleSpan";
-import {Link} from 'react-router-dom'
 
 function TheVeil() {
 	return (
-		<div>
+		<div className={'theveil'}>
 		<p>
-			Father, Odysseus lies
-			concealed at the end of the world
+			Father, Odysseus lies<br/>
+			concealed at the end of the world<br/>
  
-			Shrouded
-			seeking to live again
-
-			But in the dark at the dawn of the world
-			the archaism of the gilt prison
-			Ogygia
-			the Veil clings to him
-			the <CycleSpan entries={["various-minded","resourceful","complicated","ingenious","polytropic"]}/> peripatetic
+			Shrouded<br/>
+			seeking to live again<br/>
+			<br/>
+			But in the dark at the dawn of the world<br/>
+			the archaism of the gilt prison<br/>
+			Ogygia<br/>
+			the Veil clings to him<br/>
+			the <CycleSpan entries={["various-minded","resourceful","complicated","ingenious","polytropic"]}/> peripatetic<br/>
 			<CycleSpan entries={["two decades in tears",
 			"two decades in solitude",
-			"facing five more decades of the last two decades"]}/>
-			like night clings to shadows
-			
-			Father, extinguish the darkness
-			send him uncovered to
+			"facing five more decades of the last two decades"]}/><br/>
+			like night clings to shadows<br/>
+			<br/>
+			Father, extinguish the darkness<br/>
+			send him uncovered to<br/>
 			<CycleSpan entries={["Penelope, who kneels to blind gods",
 			"Telemachos, whose father is absence",
-			"Argos, who lives with wretched resolution"]}/>
-			
-			Ithaki lacks its brightest star
-			
-			<i>The Daughter of Endurance will give in: 
-			Odysseus' voyage must begin.</i> 
+			"Argos, who lives with wretched resolution"]}/><br/>
+			<br/>
+			Ithaki lacks its brightest star<br/>
+			<br/>
+			<i>The Daughter of Endurance will give in:<br/>
+			Odysseus' voyage must begin.<br/></i> 
 		</p>
 		</div>
 	)
